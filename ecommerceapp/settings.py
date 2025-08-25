51cdb420422ead9323498b37d7bbb70cbf211d40
Annie Brissy <annie.b@scalgo.net>
2025-08-25 13:06:29 +0530

    updations

--- ecommerceapp/settings.py
+++ ecommerceapp/settings.py
@@ -159,7 +159,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-MEDIA_URL = 'media/'
+# MEDIA_URL = 'media/'
 # MEDIA_ROOT = BASE_DIR/"media"
 
 # Default primary key field type
@@ -192,7 +192,6 @@ AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-print(AWS_ACCESS_KEY_ID, AWS_S3_CUSTOM_DOMAIN, AWS_S3_REGION_NAME, AWS_SECRET_ACCESS_KEY)
 
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
